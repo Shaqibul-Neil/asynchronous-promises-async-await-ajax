b58383f585a536ec7162ac107a0b001f0273bab6
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-02 18:26:59 +0600

    asynchronous

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const h2 = document.querySelector('h2');
+const image = document.querySelector('.img'); //synchronous way
+image.src = 'img/img-1.jpg'; //asynchronous way: setting the src of an img is loading the img in background. the code will not be blocked means other code will be executed except this one
+image.addEventListener('load', function () {
+  image.classList.add('fadeIn');
+}); //a callback function for when the img is loaded. we deferred the action for the future making the code non blocking
+h2.textContent = 'hello';
